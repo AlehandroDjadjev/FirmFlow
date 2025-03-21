fb7b103c005b2574968188cdb725a6b1d35562c5
Jeleff-cod3 <jeleffv@gmail.com>
2025-03-21 10:09:05 +0200

    fixed some tum API thing

--- backend/llm_api/views.py
+++ backend/llm_api/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from django.conf import settings
 from .models import Firm, MainDocument
 from .serializers import FirmSerializer
